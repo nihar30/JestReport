0f7586df70208af71c201ccfb5602cbd37cc77ad
shanmukhsumanth <shanmukhsumanth111@gmail.com>
2021-07-28 16:40:33 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,8 +3,8 @@ const sonarqubeScanner = require('sonarqube-scanner');
 sonarqubeScanner({
   serverUrl: 'http://18.223.255.187:9000/',
        options : {
-       'sonar.sources': '.',
-       'sonar.test': '.',
+       'sonar.sources': 'src',
+       'sonar.test': 'src',
        'sonar.inclusions' : '**',
        'sonar.javascript.jstest.reportsPath' : 'site/report',
        'sonar.javascript.lcov.reportPath' : './coverage/lcov.info'
